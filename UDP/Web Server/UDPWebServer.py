1f399316ba61e3ab1cfaae2c8fb6abc9e6154910
monanshishah <mshah88@uwo.ca>
2017-12-30 19:18:31 -0700

    Create UDPWebServer.py

---
+++ UDP/Web Server/UDPWebServer.py
@@ -0,0 +1,48 @@
+# import socket module
+from socket import *
+import sys # In order to terminate the program
+serverSocket = socket(AF_INET, SOCK_STREAM)
+# Prepare a sever socket
+
+# Fill in start
+serverPort = 6789
+serverSocket.bind(('127.0.0.1', serverPort))  # associates socket with this port
+serverSocket.listen(1)  # tells socket to listen for requests
+# Fill in end
+
+while True:
+    # Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()  # Fill in start # Fill in end
+    try:
+        message = connectionSocket.recv(1024)  # Fill in start #Fill in end
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read()  # Fill in start # Fill in end
+
+        # Send one HTTP header line into socket
+        # Fill in start
+        # httpmessage = 'HTTP/1/x 200 OK'
+        # httpmessage.encode()
+        # connectionSocket.send(bytes("HTTP/1.1 404 Not Found\r\n\r\n", "UTF-8"))
+        connectionSocket.send(bytes('HTTP/1.1 200 OK\nContent-Type: text/html\n\n', "UTF-8"))  # sends a 200 OK header line
+        # Fill in end
+        # Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
+
+        connectionSocket.close()
+    except IOError:
+        # Send response message for file not found
+        # Fill in start
+        mes = '404 File Not Found'
+        mes.encode()
+        connectionSocket.send(mes)
+        # Fill in end
+        # Close client socket
+        # Fill in start
+        connectionSocket.close()
+        # Fill in end
+    serverSocket.close()
+    sys.exit()  # Terminate the program after sending the corresponding data
